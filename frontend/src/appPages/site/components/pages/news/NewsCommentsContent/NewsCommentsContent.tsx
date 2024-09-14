f7b09e6d35e53ade53a9ab9ef5e11e9e0f8955e4
devbelek <belekasrarov10@gmail.com>
2024-09-15 00:39:02 +0600

    df

--- frontend/src/appPages/site/components/pages/news/NewsCommentsContent/NewsCommentsContent.tsx
+++ frontend/src/appPages/site/components/pages/news/NewsCommentsContent/NewsCommentsContent.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'next/navigation';
 import { useGetCommentsQuery, useAddCommentMutation, useUpdateCommentMutation, useDeleteCommentMutation, useLikeCommentMutation, useAddReplyMutation } from '@/redux/api/news';
