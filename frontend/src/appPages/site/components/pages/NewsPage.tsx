a7c02f18301bcba0db04ba2a432c88e13cc3d9b4
devbelek <belekasrarov10@gmail.com>
2024-09-14 07:04:46 +0600

    df

--- frontend/src/appPages/site/components/pages/NewsPage.tsx
+++ frontend/src/appPages/site/components/pages/NewsPage.tsx
@@ -1,14 +1,23 @@
+import { useRouter } from 'next/router';
 import NewsCommentsContent from "./news/NewsCommentsContent/NewsCommentsContent";
 import NewsMainContent from "./news/NewsMainContent/NewsMainContent";
 import scss from "./NewsPage.module.scss";
+
 const NewsPage = () => {
+  const router = useRouter();
+  const { newsId } = router.query;
+
+  const parsedNewsId = typeof newsId === 'string' ? parseInt(newsId, 10) : null;
+
+  if (parsedNewsId === null || isNaN(parsedNewsId)) {
+    return <div>Неверный идентификатор новости</div>;
+  }
+
   return (
-    <>
-      <div className={scss.content}>
-        <NewsMainContent />
-        <NewsCommentsContent />
-      </div>
-    </>
+    <div className={scss.content}>
+      <NewsMainContent newsId={parsedNewsId} />
+      <NewsCommentsContent newsId={parsedNewsId} />
+    </div>
   );
 };
 
