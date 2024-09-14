d39a1dfcc274f7b487d971dd1daaced5db953954
devbelek <belekasrarov10@gmail.com>
2024-09-14 09:14:42 +0600

    df

--- frontend/src/appPages/site/components/pages/NewsPage.tsx
+++ frontend/src/appPages/site/components/pages/NewsPage.tsx
@@ -1,15 +1,24 @@
 "use client";
 
-import NewsDetailContent from "./news/NewsDetailCotnent/NewsDetailContent";
+import NewsDetailContent from "./news/NewsDetailContent/NewsDetailContent";
+import NewsMainContent from "./news/NewsMainContent/NewsMainContent";
 import scss from "./NewsPage.module.scss";
+import { useRouter } from "next/navigation";
+
 interface NewsPageProps {
-  newsId: number;
+  newsId?: number;
 }
 
 const NewsPage: React.FC<NewsPageProps> = ({ newsId }) => {
+  const router = useRouter();
+
   return (
     <div className={scss.content}>
-      <NewsDetailContent newsId={newsId} />
+      {newsId ? (
+        <NewsDetailContent newsId={newsId} />
+      ) : (
+        <NewsMainContent />
+      )}
     </div>
   );
 };
