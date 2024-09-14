c597b4fb62f32f674773f8f78fdd44dcb5eb14a7
devbelek <belekasrarov10@gmail.com>
2024-09-14 08:58:14 +0600

    df

--- frontend/src/app/(site)/news/page.tsx
+++ frontend/src/app/(site)/news/page.tsx
@@ -1,5 +1,17 @@
+"use client"; // Это позволит использовать клиентские хуки, такие как useParams
+
+import { useParams } from 'next/navigation';
 import NewsPage from "@/appPages/site/components/pages/NewsPage";
 
-const page = () => <NewsPage />;
+const Page = () => {
+  const params = useParams();
+  const newsId = typeof params.newsDetail === 'string' ? parseInt(params.newsDetail, 10) : null;
 
-export default page;
+  if (newsId === null || isNaN(newsId)) {
+    return <div>Неверный идентификатор новости</div>;
+  }
+
+  return <NewsPage newsId={newsId} />;
+};
+
+export default Page;
