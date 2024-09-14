7ca2dd48c84515899f05ff84aaa29396e777e99b
devbelek <belekasrarov10@gmail.com>
2024-09-14 07:56:47 +0600

    df

--- frontend/src/appPages/site/components/pages/news/NewsDetailCotnent/NewsDetailContent.tsx
+++ frontend/src/appPages/site/components/pages/news/NewsDetailCotnent/NewsDetailContent.tsx
@@ -10,7 +10,7 @@ import scss from "./NewsDetailContent.module.scss";
 const NewsDetailContent: React.FC<{ newsId: number }> = ({ newsId }) => {
   const { data: commentsData, isLoading: commentsLoading, error: commentsError, refetch: refetchComments } = useGetCommentsQuery(newsId);
   const [addComment] = useAddCommentMutation();
-  const [editComment] = useEditCommentMutation();
+  const [editComment] = useUpdateCommentMutation();
   const [deleteComment] = useDeleteCommentMutation();
   const [likeComment] = useLikeCommentMutation();
 
