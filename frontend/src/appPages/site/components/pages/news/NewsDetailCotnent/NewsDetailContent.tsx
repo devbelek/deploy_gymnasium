fa818c44086c4cabf6d93b4efb7ec0be02cd32e0
devbelek <belekasrarov10@gmail.com>
2024-09-15 20:43:13 +0600

    df

--- frontend/src/appPages/site/components/pages/news/NewsDetailCotnent/NewsDetailContent.tsx
+++ frontend/src/appPages/site/components/pages/news/NewsDetailCotnent/NewsDetailContent.tsx
@@ -29,7 +29,7 @@ const NewsDetailContent: React.FC = () => {
   useEffect(() => {
     const checkAuthStatus = async () => {
       try {
-        const response = await fetch('/api/auth/user/', {
+        const response = await fetch('/accounts/user/', {
           credentials: 'include',
         });
         if (response.ok) {
