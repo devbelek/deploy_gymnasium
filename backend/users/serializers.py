59f8ac5585508c4afe4419bf474288a78d2cd2c1
devbelek <belekasrarov10@gmail.com>
2024-09-16 00:28:58 +0600

    df

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -59,6 +59,7 @@ class CommentReplySerializers(serializers.ModelSerializer):
 
     def create(self, validated_data):
         parent_comment = self.context['parent_comment']
+        validated_data.pop('parent_comment', None)
         reply = CommentReply.objects.create(parent_comment=parent_comment, **validated_data)
         return reply
 
