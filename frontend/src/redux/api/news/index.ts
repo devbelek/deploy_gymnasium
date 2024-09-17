e3a522f34fe86fc453b8748cb0d3a020f33c0fe9
devbelek <belekasrarov10@gmail.com>
2024-09-18 01:20:56 +0600

    df

--- frontend/src/redux/api/news/index.ts
+++ frontend/src/redux/api/news/index.ts
@@ -86,6 +86,13 @@ const api = index.injectEndpoints({
       }),
       invalidatesTags: ["comments"],
     }),
+    getUserProfile: build.query<NEWS.GetUserProfileResponse, string>({
+      query: (username) => ({
+        url: `${ENDPOINTS}/profile/${username}/`,
+        method: "GET",
+      }),
+      providesTags: ["profile"],
+    }),
   }),
 });
 
@@ -97,4 +104,5 @@ export const {
   useUpdateCommentMutation,
   useDeleteCommentMutation,
   useLikeCommentMutation,
+  useGetUserProfileQuery,
 } = api;
