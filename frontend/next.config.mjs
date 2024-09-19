92da5f44b959268fbdeb4736b1892bb407beb8bc
devbelek <belekasrarov10@gmail.com>
2024-09-19 18:21:50 +0600

    sd

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -6,10 +6,17 @@ const nextConfig = {
       "vermojctkdkrdsxvauwc.supabase.co",
       "3-gymnasium.kg",
       "api.dicebear.com",
-
     ],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+        port: '',
+        pathname: '/6.x/initials/svg/**',
+      },
+    ],
   },
 };
 
