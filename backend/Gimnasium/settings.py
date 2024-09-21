16db57b8ab38e8f518a41d7112ad360ec6285b1e
devbelek <belekasrarov10@gmail.com>
2024-09-22 03:11:53 +0600

    d

--- backend/Gimnasium/settings.py
+++ backend/Gimnasium/settings.py
@@ -117,7 +117,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    BASE_DIR / "/static/",
+    BASE_DIR / "static",
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
