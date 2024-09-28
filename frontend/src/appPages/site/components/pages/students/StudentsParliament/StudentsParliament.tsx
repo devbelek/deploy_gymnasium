eb03b8676b8fc46fae6191f4a884d841a4a63228
devbelek <belekasrarov10@gmail.com>
2024-09-28 20:57:13 +0600

    juma

--- frontend/src/appPages/site/components/pages/students/StudentsParliament/StudentsParliament.tsx
+++ frontend/src/appPages/site/components/pages/students/StudentsParliament/StudentsParliament.tsx
@@ -1,8 +1,18 @@
+import { useEffect, useState } from 'react';
 import Link from "next/link";
 import scss from "./StudentsParliament.module.scss";
 import { useLanguageStore } from "@/stores/useLanguageStore";
+
 const StudentsParliament = () => {
-    const { isKyrgyz, t } = useLanguageStore();
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    const { t } = useLanguageStore(state => ({
+        t: mounted ? state.t : (ky: string, ru: string) => ru
+    }));
 
     return (
         <section className={scss.content}>
@@ -11,7 +21,6 @@ const StudentsParliament = () => {
                 className={scss.ParliamBlock}
             >
                 <h1 className={scss.ParliamBlock_text}>
-                    {" "}
                     {t("Мектеп парламенти", "Школьный парламент")}
                 </h1>
             </Link>
--- frontend/src/stores/useLanguageStore.tsx
+++ frontend/src/stores/useLanguageStore.tsx
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 type LanguageStore = {
   isKyrgyz: boolean;
@@ -6,8 +7,16 @@ type LanguageStore = {
   t: (ky: string, ru: string) => string;
 };
 
-export const useLanguageStore = create<LanguageStore>((set, get) => ({
-  isKyrgyz: true,
-  setIsKyrgyz: (isKyrgyz: boolean) => set({ isKyrgyz }),
-  t: (ky: string, ru: string) => (get().isKyrgyz ? ky : ru),
-}));
+export const useLanguageStore = create<LanguageStore>()(
+  persist(
+    (set, get) => ({
+      isKyrgyz: true,
+      setIsKyrgyz: (isKyrgyz: boolean) => set({ isKyrgyz }),
+      t: (ky: string, ru: string) => (get().isKyrgyz ? ky : ru),
+    }),
+    {
+      name: "language-storage",
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
