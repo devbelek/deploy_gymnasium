3242b471e7290c5d219983915bc0c6fae4286d46
devbelek <belekasrarov10@gmail.com>
2024-10-01 15:10:51 +0600

    sdfsdf

--- backend/Gimnasium/settings.py
+++ backend/Gimnasium/settings.py
@@ -58,10 +58,16 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+    'https://3-gymnasium.kg'
 ]
 
 
