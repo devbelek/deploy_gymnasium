30017c57b3e9f331bb53bc5d4b7edd8e9ef58f1e
devbelek <belekasrarov10@gmail.com>
2024-10-02 15:56:38 +0600

    commit

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,32 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: "http",
-                hostname: "3-gymnasium.kg",
-                port: "",
-                pathname: "/media/**", // Changed to cover both /media/images/ and /media/avatars/
-            },
-            {
-                protocol: "https",
-                hostname: "gimnasium.onrender.com",
-                port: "",
-                pathname: "/**",
-            },
-            {
-                protocol: "https",
-                hostname: "vermojctkdkrdsxvauwc.supabase.co",
-                port: "",
-                pathname: "/**",
-            },
-            {
-                protocol: "https",
-                hostname: "api.dicebear.com",
-                port: "",
-                pathname: "/**",
-            },
-        ],
+        dangerouslyAllowSVG: true,
+        domains: ['api.dicebear.com', '3-gymnasium.kg', 'vermojctkdkrdsxvauwc.supabase.co'],
+        },
     },
 };
 
