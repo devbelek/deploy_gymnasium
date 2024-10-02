2a4e59a3bc360627eead8b08f50a42be4a9512b6
devbelek <belekasrarov10@gmail.com>
2024-10-02 16:22:05 +0600

    commit

--- frontend/src/appPages/site/components/pages/home/HeroContent/HeroContent.tsx
+++ frontend/src/appPages/site/components/pages/home/HeroContent/HeroContent.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import scss from "./HeroContent.module.scss";
-import logoImage from "../../../../../../assets/images/logo.jpg"; // Убедитесь, что путь к логотипу правильный
+import heroImage from "../../../../../../assets/images/logo.jpg";
 import { useLanguageStore } from "@/stores/useLanguageStore";
 
 const HeroContent = () => {
@@ -10,11 +10,11 @@ const HeroContent = () => {
   return (
     <section className={scss.content}>
       <div className={scss.backgroundImage}></div>
-      <div className={scss.textContainer}>
-        <div className={scss.logo}>
-          <Image src={logoImage} alt="Логотип" width={100} height={100} />
-        </div>
+      <div className={scss.contentWrapper}>
         <div className={scss.text}>
+          <div className={scss.logo}>
+            <Image src={heroImage} alt="Logo" width={100} height={100} />
+          </div>
           <h1>
             {t(
               "№3 Эксперименталдык гимназия",
