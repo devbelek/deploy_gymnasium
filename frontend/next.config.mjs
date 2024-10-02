e7de570c82985a336ca81058237a980fd6794d70
devbelek <belekasrarov10@gmail.com>
2024-10-02 16:37:09 +0600

    commit

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,7 +2,23 @@
 const nextConfig = {
     images: {
         dangerouslyAllowSVG: true,
-        domains: ['api.dicebear.com', '3-gymnasium.kg', 'vermojctkdkrdsxvauwc.supabase.co'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'api.dicebear.com',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: '3-gymnasium.kg',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'vermojctkdkrdsxvauwc.supabase.co',
+                pathname: '/**',
+            },
+        ],
     },
 };
 
