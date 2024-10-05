16f570befb5e60e96500034b1bb92d84482d339a
devbelek <belekasrarov10@gmail.com>
2024-10-05 06:45:50 +0600

    dffd

--- frontend/src/appPages/site/components/pages/gallery/GalleryMainContent/GalleryMainContent.tsx
+++ frontend/src/appPages/site/components/pages/gallery/GalleryMainContent/GalleryMainContent.tsx
@@ -16,7 +16,7 @@ const getImageUrl = (imageUrl: string) => {
 };
 
 const getYoutubeId = (url: string) => {
-  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
   const match = url.match(regExp);
   return (match && match[2].length === 11) ? match[2] : null;
 };
