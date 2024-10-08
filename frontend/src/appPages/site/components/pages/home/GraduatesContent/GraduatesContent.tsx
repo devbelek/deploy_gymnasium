e94905a3712b727fa73542d012a5fcf09bf82eee
rahmanovdev <jumadilrahmonov@gmail.com>
2024-10-09 02:03:23 +0600

    juma

--- frontend/src/appPages/site/components/pages/home/GraduatesContent/GraduatesContent.tsx
+++ frontend/src/appPages/site/components/pages/home/GraduatesContent/GraduatesContent.tsx
@@ -5,12 +5,17 @@ import { GrLinkNext, GrLinkPrevious } from "react-icons/gr";
 import { useGetSuccessfulGraduatesQuery } from "@/redux/api/successful_graduates";
 import graduateFallback from "../../../../../../assets/images/Group 1000001472.png";
 import { useLanguageStore } from "@/stores/useLanguageStore";
+import { useRouter } from "next/navigation";
 
 const GraduatesContent = () => {
   const { data } = useGetSuccessfulGraduatesQuery();
   const { isKyrgyz, t } = useLanguageStore();
 
   const graduateData = data && data.length > 0 ? data[0] : null;
+  const router = useRouter();
+  const handleNavigate = () => {
+    router.push("/graduates");
+  };
 
   return (
     <section className={scss.content}>
@@ -47,7 +52,7 @@ const GraduatesContent = () => {
         </div>
         <div className={scss.buttonContainer}>
           <button className={scss.button} onClick={handleNavigate}>
-            {t("Бүтүндөй галерея", "Вся галерея")}
+            {t("Жалпы бүтүрүүчүлөр", "Все выпускники")}
           </button>
         </div>
       </div>
