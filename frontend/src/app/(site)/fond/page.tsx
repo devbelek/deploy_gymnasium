f4d7b2e332406d03aca07a5b2e474fa22bb15e50
devbelek <belekasrarov10@gmail.com>
2024-10-11 15:19:34 +0600

    dfdf

--- frontend/src/app/(site)/fond/page.tsx
+++ frontend/src/app/(site)/fond/page.tsx
@@ -9,7 +9,9 @@ export const metadata: Metadata = {
     title: "Фонд N3 Экспериментальной гимназии",
     description: "Поддержка талантливых учеников и развитие образования",
   },
-  canonical: "https://3-gymnasium.kg/fond/",
+  alternates: {
+    canonical: "https://3-gymnasium.kg/fond/",
+  },
 };
 
 const page = () => <FondPage />;
