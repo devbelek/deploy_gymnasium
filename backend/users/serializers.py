0dae0bd70f2e083bbe9ca6aea8608cbbfa83d4f5
devbelek <belekasrarov10@gmail.com>
2024-10-15 07:50:53 +0600

    sdf

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import UserProfile, Comment, CommentReply, Like, ConfirmedDonation, Donation, DonationRequisite
+from .models import UserProfile, Comment, CommentReply, Like, Donation, DonationRequisite
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
