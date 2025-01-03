cb901ff2ef0354765d41ddf678f458cb65f3b81e
devbelek <belekasrarov10@gmail.com>
2025-01-03 06:17:49 +0600

    sssss

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,64 +2,63 @@ import withBundleAnalyzer from '@next/bundle-analyzer';
 import withPWA from 'next-pwa';
 
 const config = {
-  poweredByHeader: false,
-  compress: true,
+ output: 'standalone',
+ poweredByHeader: false,
+ compress: true,
 
-  images: {
-    domains: ['3-gymnasium.kg'],
-    minimumCacheTTL: 60,
-    formats: ['image/avif', 'image/webp'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256],
-    loader: 'default',
-    unoptimized: false
-  },
+ images: {
+   domains: ['3-gymnasium.kg'],
+   minimumCacheTTL: 60,
+   formats: ['image/avif', 'image/webp'],
+   deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+   imageSizes: [16, 32, 48, 64, 96, 128, 256],
+   loader: 'default',
+   unoptimized: false
+ },
 
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        'react': 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      });
-    }
+ webpack: (config, { dev, isServer }) => {
+   if (!dev && !isServer) {
+     Object.assign(config.resolve.alias, {
+       'react': 'preact/compat',
+       'react-dom/test-utils': 'preact/test-utils',
+       'react-dom': 'preact/compat',
+     });
+   }
 
-    config.optimization = {
-      ...config.optimization,
-      minimize: !dev,
-      minimizer: [
-        ...config.optimization.minimizer || [],
-      ],
-      splitChunks: {
-        chunks: 'all',
-        maxInitialRequests: 25,
-        minSize: 20000,
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name(module) {
-              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-              return `vendor.${packageName.replace('@', '')}`;
-            },
-          },
-        },
-      },
-      runtimeChunk: 'single'
-    };
+   config.optimization = {
+     ...config.optimization,
+     minimize: !dev,
+     minimizer: [
+       ...config.optimization.minimizer || [],
+     ],
+     splitChunks: {
+       chunks: 'all',
+       maxInitialRequests: 25,
+       minSize: 20000,
+       cacheGroups: {
+         vendor: {
+           test: /[\\/]node_modules[\\/]/,
+           name(module) {
+             const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+             return `vendor.${packageName.replace('@', '')}`;
+           },
+         },
+       },
+     },
+     runtimeChunk: 'single'
+   };
 
-    return config;
-  },
+   return config;
+ },
 
-  experimental: {
-    optimizeCss: true,
-    scrollRestoration: true,
-    workerThreads: true,
-    legacyBrowsers: false,
-    serverActions: true,
-    typedRoutes: true
-  }
+ experimental: {
+   optimizeCss: true,
+   scrollRestoration: true,
+   workerThreads: true,
+   legacyBrowsers: false,
+   serverActions: true,
+   typedRoutes: true
+ }
 };
 
-export default withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true'
-})(withPWA(config));
+export default config;
