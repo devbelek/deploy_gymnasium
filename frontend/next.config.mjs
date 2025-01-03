7af1c3972f46f971a8c6353163882316b931687c
devbelek <belekasrarov10@gmail.com>
2025-01-03 20:38:03 +0600

    sssss

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -6,24 +6,28 @@ const config = {
   images: {
     remotePatterns: [
       {
+        protocol: 'http',
+        hostname: '3-gymnasium.kg'
+      },
+      {
         protocol: 'https',
-        hostname: '3-gymnasium.kg',
-        port: '',
-        pathname: '/**'
+        hostname: '3-gymnasium.kg'
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com'
       }
     ],
     minimumCacheTTL: 60,
     formats: ['image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256],
-    path: '/_next/image',
-    dangerouslyAllowSVG: true
+    imageSizes: [16, 32, 48, 64, 96, 128, 256]
   },
 
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': './src'  // Изменили на относительный путь
+      '@': './src'
     };
     return config;
   },
