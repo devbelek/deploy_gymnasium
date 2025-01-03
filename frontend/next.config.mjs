ffa0e9ffb8d64951f518c476b2b96c02c3b7d0d1
devbelek <belekasrarov10@gmail.com>
2025-01-03 19:48:26 +0600

    sssss

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,21 +1,29 @@
-// next.config.mjs
 const config = {
   output: 'standalone',
   poweredByHeader: false,
   compress: true,
 
   images: {
-    domains: ['3-gymnasium.kg'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '3-gymnasium.kg',
+        port: '',
+        pathname: '/**'
+      }
+    ],
     minimumCacheTTL: 60,
-    formats: ['image/avif', 'image/webp'],
+    formats: ['image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256]
+    imageSizes: [16, 32, 48, 64, 96, 128, 256],
+    path: '/_next/image',
+    dangerouslyAllowSVG: true
   },
 
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': '/app/src'
+      '@': './src'  // Изменили на относительный путь
     };
     return config;
   },
